a16509e834211647b1b82ce8b1dbbed690c298d3
Michael Darche <mkdarche@gmail.com>
2019-08-01 18:42:53 -0400

    begin docs

---
+++ docs/gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: "Gatsby Theme Elements Documentation",
+    description: "The official doc site for Gatsby Theme Elements",
+    siteUrl: "https://mikedarche.com",
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-theme-ui`,
+    `gatsby-theme-elements`,
+  ],
+}
---
+++ docs/src/pages/index.js
@@ -0,0 +1,11 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import { Layout } from "gatsby-theme-elements"
+
+const IndexPage = () => (
+  <div sx={{ color: "red" }}>
+    <Layout>Test</Layout>
+  </div>
+)
+
+export default IndexPage
