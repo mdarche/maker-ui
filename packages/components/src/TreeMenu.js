0517a0f74dfdbb1aba2ee6dddc34b1c7a2f7c7ba
mdarche <mkdarche@gmail.com>
2020-03-08 23:20:14 -0400

    Update tree menu

--- packages/components/src/TreeMenu.js
+++ packages/components/src/TreeMenu.js
@@ -1,113 +1,150 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Box } from 'theme-ui'
-import ResizeObserver from 'resize-observer-polyfill'
 import { useSpring, animated as a } from 'react-spring'
 
-// Helpers
+import { useMeasure, usePrevious } from './helper'
+import { MinusIcon, PlusIcon, CloseIcon } from './icons'
 
-export function usePrevious(value) {
-  const ref = useRef()
-  useEffect(() => void (ref.current = value), [value])
-  return ref.current
-}
-
-export function useMeasure() {
-  const ref = useRef()
-  const [bounds, set] = useState({ left: 0, top: 0, width: 0, height: 0 })
-  const [ro] = useState(
-    () => new ResizeObserver(([entry]) => set(entry.contentRect))
-  )
-  useEffect(() => {
-    if (ref.current) ro.observe(ref.current)
-    return () => ro.disconnect()
-  }, [])
-  return [{ ref }, bounds]
-}
-
-// Components
-
-const Tree = React.memo(({ children, name, style, defaultOpen = false }) => {
-  const [isOpen, setOpen] = useState(defaultOpen)
+const Tree = React.memo(({ children, name, style, open = false }) => {
+  const [isOpen, setOpen] = useState(open)
   const previous = usePrevious(isOpen)
   const [bind, { height: viewHeight }] = useMeasure()
 
-  const { height, opacity, transform } = useSpring({
-    from: { height: 0, opacity: 0, transform: 'translate3d(20px,0,0)' },
+  const { height } = useSpring({
+    from: { height: 0 },
     to: {
       height: isOpen ? viewHeight : 0,
-      opacity: isOpen ? 1 : 0,
-      transform: `translate3d(${isOpen ? 0 : 20}px,0,0)`,
     },
   })
 
-  // const Icon = Icons[`${children ? (isOpen ? 'Minus' : 'Plus') : 'Close'}SquareO`]
-
   return (
     <Box
+      className="tree-item"
       sx={{
         position: 'relative',
-        pt: '4px',
+        pt: '5px',
+        pl: '5px',
         textOverflow: 'ellipsis',
-        whiteSpace: 'no-wrap',
-        overflowX: 'hidden',
+        whiteSpace: 'nowrap',
         verticalAlign: 'middle',
       }}>
-      <button onClick={() => setOpen(!isOpen)}>
-        action
-        {/* {children ? (isOpen ? '-' : '+') : 'x'} */}
-      </button>
+      <Box
+        as="button"
+        onClick={() => setOpen(!isOpen)}
+        sx={{
+          background: 'none',
+          border: 'none',
+          p: 0,
+          mr: '10px',
+          cursor: 'pointer',
+          color: 'primary',
+          svg: {
+            height: '16px',
+            fill: 'currentColor',
+          },
+          verticalAlign: 'middle',
+        }}>
+        {children ? isOpen ? <MinusIcon /> : <PlusIcon /> : <CloseIcon />}
+      </Box>
       <Box as="span" style={style}>
         {name}
       </Box>
       <a.div
         style={{
-          willChange: 'opacity, transform, height',
-          marginLeft: 6,
-          paddingBottom: 15,
+          willChange: 'height',
+          paddingLeft: 10,
           overflow: 'hidden',
-          opacity,
           height: isOpen && previous === isOpen ? 'auto' : height,
         }}>
-        <a.div style={{ transform }} {...bind} children={children} />
+        <Box {...bind}>{children}</Box>
       </a.div>
     </Box>
   )
 })
 
-const TreeMenu = React.forwardRef((props, ref) => {
+// TODO - This needs to be constructed with a visual editor or with MDX style syntax
+
+const data = [
+  {
+    label: 'main',
+    path: '',
+    component: '',
+    submenu: [
+      {
+        label: 'subtree with children',
+        path: '',
+        component: '',
+        submenu: [
+          {
+            label: 'hello',
+            path: '',
+            component: '',
+            submenu: [
+              { label: 'Layer 3', path: '', component: '', submenu: [] },
+            ],
+          },
+          {
+            label: 'hi',
+            path: '',
+            component: '',
+            submenu: [
+              { label: 'Layer Layer 3', path: '', component: '', submenu: [] },
+            ],
+          },
+        ],
+      },
+      { label: 'hi', path: '', component: '', submenu: [] },
+    ],
+  },
+]
+
+// Use this config syntax for code
+const menu = `
+root
+- branch
+- branch
+- branch
+  - nested
+  - nested
+    - deeper
+    - deeper
+      - deepest
+  -nested
+  -nested
+`
+
+const TreeMenu = React.forwardRef(({ variant = 'tree', ...props }, ref) => {
   return (
-    <Tree name="main" defaultOpen>
-      <Tree name="hello" />
-      <Tree name="subtree with children">
-        <Tree name="hello" />
-        <Tree name="sub-subtree with children">
-          <Tree name="child 1" style={{ color: '#37ceff' }} />
-          <Tree name="child 2" style={{ color: '#37ceff' }} />
-          <Tree name="child 3" style={{ color: '#37ceff' }} />
-          <Tree name="custom content">
-            <div
-              style={{
-                position: 'relative',
-                width: '100%',
-                height: 200,
-                padding: 10,
-              }}>
-              <div
-                style={{
-                  width: '100%',
-                  height: '100%',
-                  background: 'black',
-                  borderRadius: 5,
-                }}
-              />
-            </div>
-          </Tree>
+    <Box ref={ref}>
+      {/* {data.map((a, index) => (
+        <Tree name={a.label}>
+          {a.submenu.map((b, index) => (
+            <Tree name={b.label}>
+              {b.submenu.map((c, index) => (
+                <Tree name={c.label} />
+              ))}
+            </Tree>
+          ))}
         </Tree>
+      ))} */}
+      <Tree name="main" open>
         <Tree name="hello" />
+        <Tree name="subtree with children">
+          <Tree name="hello" />
+          <Tree name="sub-subtree with children">
+            <Tree name="child 1" />
+            <Tree name="child 2" />
+            <Tree name="child 3" />
+            <Tree name="custom content">
+              <div>Test!</div>
+            </Tree>
+          </Tree>
+          <Tree name="hello" />
+        </Tree>
+        <Tree name="world" />
+        <Tree name={<span>something something</span>} />
       </Tree>
-      <Tree name="world" />
-      <Tree name={<span>🙀 something something</span>} />
-    </Tree>
+    </Box>
   )
 })
 
---
+++ packages/components/src/helper.js
@@ -0,0 +1,21 @@
+import React, { useState, useRef, useEffect } from 'react'
+import ResizeObserver from 'resize-observer-polyfill'
+
+export function usePrevious(value) {
+  const ref = useRef()
+  useEffect(() => void (ref.current = value), [value])
+  return ref.current
+}
+
+export function useMeasure() {
+  const ref = useRef()
+  const [bounds, set] = useState({ left: 0, top: 0, width: 0, height: 0 })
+  const [ro] = useState(
+    () => new ResizeObserver(([entry]) => set(entry.contentRect))
+  )
+  useEffect(() => {
+    if (ref.current) ro.observe(ref.current)
+    return () => ro.disconnect()
+  }, [])
+  return [{ ref }, bounds]
+}
