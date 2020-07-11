7013c0e9d8392cbb2c9e3bafb963dad87fa05f88
mdarche <mkdarche@gmail.com>
2020-07-11 13:43:31 -0400

    Update root tsconfig

--- packages/maker-ui/src/index.ts
+++ packages/maker-ui/src/index.ts
@@ -1,6 +1,6 @@
 export * from './components'
 export { useMakerUI } from './utils/hooks'
 
-// For internal use in other @maker-ui libraries
+// For internal use in other @maker-ui packages
 
 export { setBreakpoint, generateId } from './utils/helper'
