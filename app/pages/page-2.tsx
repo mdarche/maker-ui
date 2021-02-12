1dede978aec165625b86fe7aab14ceec3fc5dd73
Mike Darche <mdarche@users.noreply.github.com>
2021-02-12 11:33:33 -0500

    Add next app for cypress testing

---
+++ app/pages/page-2.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Page2() {
+  return (
+    <>
+      <h1>Page 2</h1>
+      <p>Welcome to Page 2 of Maker UI's NextJS example</p>
+      <Link href="/">
+        <a>Go to back to Home Page</a>
+      </Link>
+    </>
+  )
+}
