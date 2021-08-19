c16438d010329f820aeabc15a077d5dc172dfee0
Mike Darche <mdarche@users.noreply.github.com>
2021-08-19 01:29:46 -0400

    temporary repeater cleanup

--- packages/forms/src/Fields/Repeater.tsx
+++ packages/forms/src/Fields/Repeater.tsx
@@ -1,72 +1,54 @@
 import * as React from 'react'
+import { Flex } from 'maker-ui'
 import { useFormikContext, Field, FieldArray, FormikErrors } from 'formik'
+
+import { TrashIcon, PlusIcon } from '../Icons'
 
 // Todo
 export const Repeater = () => {
-  const { errors, values } = useFormikContext()
+  const {
+    errors,
+    values,
+  }: { errors: FormikErrors<any>; values: any } = useFormikContext()
+  const valueKey = 'todo'
+
+  console.log('Errors are', errors)
 
   return (
     <FieldArray
-      name="products"
+      name={valueKey}
       render={({ remove, push }) => (
         <div>
-          {values.products.length > 0 &&
-            values.products.map((p, index) => {
+          {values[valueKey].length > 0 &&
+            values[valueKey].map((p: any, index: number) => {
+              console.log(p)
               return (
                 <div key={index}>
                   <Flex className="row">
-                    <Div>
-                      <Div className="custom-select" css={{ marginRight: 15 }}>
-                        <Field as="select" name={`products[${index}].name`}>
-                          <option>Select a product</option>
-                          {Object.keys(order.inventory)
-                            .sort(sortAlphaNum)
-                            .map(key => (
-                              <option key={key}>{key}</option>
-                            ))}
-                        </Field>
-                        <DownIcon className="down-caret" />
-                        <Div css={{ display: 'none' }}>
-                          <Field
-                            name={`products[${index}].pallets`}
-                            type="number"
-                            value={
-                              (values.products[index].pallets = pallets || 0)
-                            }
-                          />
-                        </Div>
-                      </Div>
-                    </Div>
-                    <Div>
-                      <Div>
-                        <Div className="sqft-wrapper">
-                          <Field
-                            className="sqft"
-                            name={`products[${index}].sqft`}
-                            placeholder="24"
-                            type="number"
-                            min="0"
-                          />
-                        </Div>
-                      </Div>
-                    </Div>
-                    <Button
+                    <Field
+                      className="sqft"
+                      name={`products[${index}].sqft`}
+                      placeholder="24"
+                      type="number"
+                      min="0"
+                    />
+                    <button
                       type="button"
                       className="btn-remove"
                       onClick={() => remove(index)}>
                       <TrashIcon />
-                    </Button>
+                    </button>
                   </Flex>
                 </div>
               )
             })}
           <Flex justify="flex-start">
-            <Button
+            <button
               type="button"
               className="form-repeater-btn"
               onClick={() => push({ name: '', sqft: '' })}>
               <PlusIcon /> Add
-            </Button>
+            </button>
           </Flex>
         </div>
       )}
