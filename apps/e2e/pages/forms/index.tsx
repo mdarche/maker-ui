3710c9215d17478c89605de73107bcb03fc9b1c4
Mike Darche <mdarche@users.noreply.github.com>
2022-03-24 10:48:46 -0400

    Link packages via pnpm workspace

--- apps/e2e/pages/forms/index.tsx
+++ apps/e2e/pages/forms/index.tsx
@@ -23,7 +23,7 @@ const testFields: FieldProps[] = [
     type: 'select',
     initialValue: [],
     settings: {
-      isCreatableInput: true,
+      // isCreatableInput: true,
       isMulti: true,
       placeholder: 'Select',
       options: [
