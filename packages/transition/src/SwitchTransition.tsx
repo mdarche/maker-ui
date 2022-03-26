b32ecec0f16071aca516885255ef1a161381f769
Mike Darche <mdarche@users.noreply.github.com>
2022-03-26 09:56:27 -0400

    Begin switch transition

--- packages/transition/src/SwitchTransition.tsx
+++ packages/transition/src/SwitchTransition.tsx
@@ -1,5 +1,12 @@
 export interface SwitchTransitionProps {}
 
 export const SwitchTransition = () => {
-  return <div>Transition</div>
+  return <div ref={nodeRef}></div>
+  // <SwitchTransition mode="out-in">
+  // <CSSTransition
+  //   nodeRef={nodeRef}
+  //   classNames="fade"
+  //   key={series1 ? 'series1' : 'series2'}
+  //   timeout={200}
+  //   unmountOnExit>
 }
