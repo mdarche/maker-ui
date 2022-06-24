86f2aa4b7ccece67a27353b99c230344344d0a75
Naveen Tiwari <navs.hbk@gmail.com>
2022-06-24 19:01:26 +0530

    commit test

--- cypress/integration/image-picker.spec.tsx
+++ cypress/integration/image-picker.spec.tsx
@@ -4,36 +4,20 @@ describe('Forms Basic screen',()=>{
   it('Page load test',()=>{
     cy.visit('/forms/image-picker');
 
-    cy.get("[data-cy=username]").should('exist');
+    cy.get("[data-cy=myImage]").should('exist');
   })
  
   
 
     it('Testing picture uploading', () => {
-    // cy.fixture('test-image.jpg').then(fileContent => {
-    //     cy.get("[data-cy=profileImage]").attachFile({
-    //         fileContent: fileContent.toString(),
-    //         fileName: 'test-image.jpg',
-    //         mimeType: 'image/jpg'
-    //         });
-    //     });
 
         const filepath = 'test-image.jpg'
-        cy.get('[data-cy=profileImage]').attachFile(filepath)
+        cy.get('.hidden').attachFile(filepath)
        // cy.get('#file-submit').click()
         //cy.get('#uploaded-files').contains('evening.png')
 
         cy.get(".btn-remove").should('exist');
 
-    });
-
-      it('Pick an option',()=>{
-       cy.get("[id='mySelect']").should('exist');
-       cy.get("[id='mySelect']").type('Yes')
-      .type('{enter}');
-      //departments
-    });
-
-    
+    });    
 
 })
