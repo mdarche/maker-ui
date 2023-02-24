9c0e564f602e58e98c0e475567f051d650d86344
Mike Darche <mdarche@users.noreply.github.com>
2023-02-24 15:55:32 -0500

    Finish @maker-ui/hooks tests

--- packages/hooks/src/useStorage.ts
+++ packages/hooks/src/useStorage.ts
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import { merge } from '@maker-ui/utils'
 
 type StorageType = 'session' | 'local' | 'cookie'
 
@@ -29,66 +28,63 @@ export type StorageProps = {
  * @returns {string | false} - The value of the storage item or false if it has expired
  *
  */
-export function useStorage(props: StorageProps): string | object | false {
-  const now = new Date()
-  const { key, value, type, expires, expireDays } = merge(
-    { type: 'local', expireDays: 30 },
-    props
-  ) as StorageProps
-
-  const exp =
-    expires || now.getTime() + (expireDays as number) * 24 * 60 * 60 * 1000
-
+export function useStorage({
+  key,
+  value,
+  type = 'local',
+  expires,
+  expireDays = 30,
+}: StorageProps) {
+  const getItem = () =>
+    type === 'session'
+      ? getSessionStorage(key)
+      : type === 'local'
+      ? getLocalStorage(key)
+      : getCookie(key)
   const [saved, setSaved] = useState<string | null>(() => getItem())
-
-  function getItem() {
+  const exp = expires || new Date().getTime() + expireDays * 24 * 60 * 60 * 1000
+  const setItem = () => {
+    if (!value) return
+    const v = typeof value === 'object' ? JSON.stringify(value) : value
     if (type === 'session') {
-      return getSessionStorage(key)
+      window.sessionStorage.setItem(key, v)
     }
     if (type === 'local') {
-      return getLocalStorage(key)
+      window.localStorage.setItem(key, JSON.stringify({ value, expires: exp }))
     }
-    return getCookie(key)
+    if (type === 'cookie') {
+      const expDate = new Date(exp).toUTCString()
+      document.cookie = `${key}=${v};expires=${expDate};path=/`
+    }
+    setSaved(v)
   }
 
   useEffect(() => {
     if (saved !== null) return
-    if (value) {
-      const v = typeof value === 'object' ? JSON.stringify(value) : value
-      if (type === 'session') {
-        window.sessionStorage.setItem(key, v)
-      }
-      if (type === 'local') {
-        localStorage.setItem(key, JSON.stringify({ value, expires: exp }))
-      }
-      if (type === 'cookie') {
-        const expDate = new Date(exp).toUTCString()
-        document.cookie = `${key}=${v};expires=${expDate};path=/`
-      }
-
-      setSaved(v)
-    }
-
+    setItem()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [saved])
 
   useEffect(() => {
     if (saved === null) return
-    setSaved(getItem())
+    if (value && value !== parseItem(saved)) {
+      setItem()
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [key, type])
-
+  }, [value, type])
   // Format back to object if necessary, otherwise return string
+  return parseItem(saved)
+}
+
+const parseItem = (item?: string | null): string | object | false => {
   try {
-    return saved ? JSON.parse(saved) : false
+    return item ? JSON.parse(item) : false
   } catch (err) {
-    return saved || false
+    return item || false
   }
 }
 
-function isExpired(expires: number | string) {
-  return new Date(expires) < new Date()
-}
+const isExpired = (expires: number | string) => new Date(expires) < new Date()
 
 function getSessionStorage(key: string) {
   const item = window.sessionStorage.getItem(key)
@@ -99,7 +95,10 @@ function getLocalStorage(key: string) {
   const item = window.localStorage.getItem(key)
   if (!item) return null
   const { value, expires } = JSON.parse(item)
-  if (isExpired(expires)) return null
+  if (isExpired(expires)) {
+    window.localStorage.removeItem(key)
+    return null
+  }
   return JSON.stringify(value)
 }
 
@@ -107,7 +106,7 @@ function getCookie(key: string) {
   const cookies = document.cookie.split(';').map((cookie) => cookie.trim())
   const cookie = cookies.find((cookie) => cookie.startsWith(`${key}=`))
   if (!cookie) return null
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // eslint-disable-next-line
   const [_, value, expires] = cookie.split('=')
   if (isExpired(expires)) return null
   return value
--- packages/hooks/tests/useStorage.cy.tsx
+++ packages/hooks/tests/useStorage.cy.tsx
@@ -3,26 +3,33 @@ import { useStorage, type StorageProps } from '../src/useStorage'
 
 interface Props {
   config: StorageProps
+  toggleValue?: boolean
 }
 
 export const StorageComponent = ({
   config: { key, value, type, expires, expireDays },
+  toggleValue = false,
 }: Props) => {
+  const [val, setVal] = React.useState(value)
   const stored = useStorage({
     key,
-    value,
+    value: val,
     type,
     expires,
     expireDays,
   })
-  console.log('stored value is', stored)
   return (
-    <div data-cy="store">
-      {stored
-        ? typeof stored === 'string'
-          ? stored
-          : JSON.stringify(stored)
-        : 'initial'}
+    <div>
+      <div data-cy="store">
+        {stored
+          ? typeof stored === 'string'
+            ? stored
+            : JSON.stringify(stored)
+          : 'initial'}
+      </div>
+      {toggleValue && (
+        <button onClick={() => setVal('new')}>Change Value</button>
+      )}
     </div>
   )
 }
@@ -32,36 +39,124 @@ describe('useStorage', () => {
   const value = 'myValue'
   const valueObject = { nested: 'myValue' }
 
+  // LOCAL STORAGE
   describe('local', () => {
-    // Also test expiration
+    it('returns false when item does not exist and no value is provided', () => {
+      cy.window().then((win) => win.localStorage.clear())
+      cy.mount(<StorageComponent config={{ key }} />)
+      cy.wait(200)
+      cy.get('[data-cy="store"]').should('have.text', 'initial')
+    })
+
+    it('saves to local storage when item does not exist and value is provided', () => {
+      const defaultExpiry = new Date().getTime() + 30 * 24 * 60 * 60 * 1000 // 30 days
+      cy.mount(<StorageComponent config={{ key, value }} />)
+      cy.wait(200)
+      cy.get('[data-cy="store"]').should('have.text', value)
+      // Check for the default expiry within 2 second range
+      cy.window().then((w) => {
+        console.log(w.localStorage.getItem(key))
+        //@ts-ignore
+        cy.wrap(JSON.parse(w.localStorage.getItem(key)).expires).should(
+          'be.within',
+          defaultExpiry - 2000,
+          defaultExpiry + 2000
+        )
+      })
+    })
+
+    it('saves and serializes an object to local storage', () => {
+      cy.window().then((win) => win.localStorage.clear())
+      cy.window().then((w) => {
+        cy.wrap(w.localStorage.getItem(key)).should('not.exist')
+      })
+      cy.mount(<StorageComponent config={{ key, value: valueObject }} />)
+      cy.wait(200)
+      cy.get('[data-cy="store"]').should(
+        'have.text',
+        JSON.stringify(valueObject)
+      )
+    })
+
+    it('updates the local storage item when the value changes', () => {
+      cy.mount(
+        <StorageComponent config={{ key, value, type: 'local' }} toggleValue />
+      )
+      cy.wait(200)
+      cy.get('[data-cy="store"]').should('have.text', value)
+      cy.get('button').click()
+      cy.get('[data-cy="store"]').should('have.text', 'new')
+    })
+
+    it('returns false and deletes the item when it expires and no value is present', () => {
+      cy.window().then((w) => window.localStorage.clear())
+      cy.mount(
+        <StorageComponent
+          config={{
+            key,
+            value,
+            expires: new Date().getTime() + 500,
+          }}
+        />
+      )
+      cy.get('[data-cy="store"]').should('have.text', value)
+      cy.wait(1000)
+      cy.mount(<StorageComponent config={{ key, type: 'local' }} />)
+      cy.get('[data-cy="store"]').should('have.text', 'initial')
+      cy.window().then((w) => {
+        cy.wrap(w.localStorage.getItem(key)).should('not.exist')
+      })
+    })
+
+    it('sets a new value with expiry when item has expired and value is present', () => {
+      cy.window().then((w) => window.localStorage.clear())
+      cy.mount(
+        <StorageComponent
+          config={{
+            key,
+            value,
+            expires: new Date().getTime() + 500,
+          }}
+        />
+      )
+      cy.get('[data-cy="store"]').should('have.text', value)
+      cy.wait(1000)
+      const newExpire = new Date().getTime() + 10000
+      cy.mount(
+        <StorageComponent
+          config={{ key, value: 'new', type: 'local', expires: newExpire }}
+        />
+      )
+      cy.get('[data-cy="store"]').should('have.text', 'new')
+      cy.window().then((w) => {
+        //@ts-ignore
+        cy.wrap(JSON.parse(w.localStorage.getItem(key)).expires).should(
+          'eq',
+          newExpire
+        )
+      })
+    })
   })
 
+  // SESSION STORAGE
   describe('session', () => {
     beforeEach(() => {
       cy.window().then((win) => win.sessionStorage.clear())
     })
+
     it('returns false when item does not exist and no value is provided', () => {
-      cy.window().then((w) => {
-        cy.wrap(w.sessionStorage.getItem(key)).should('not.exist')
-      })
       cy.mount(<StorageComponent config={{ key, type: 'session' }} />)
       cy.wait(200)
       cy.get('[data-cy="store"]').should('have.text', 'initial')
     })
 
     it('saves to session storage when item does not exist and value is provided', () => {
-      cy.window().then((w) => {
-        cy.wrap(w.sessionStorage.getItem(key)).should('not.exist')
-      })
       cy.mount(<StorageComponent config={{ key, value, type: 'session' }} />)
       cy.wait(200)
       cy.get('[data-cy="store"]').should('have.text', value)
     })
 
-    it('saves and serializes an object to storage', () => {
-      cy.window().then((w) => {
-        cy.wrap(w.sessionStorage.getItem(key)).should('not.exist')
-      })
+    it('saves and serializes an object to session storage', () => {
       cy.mount(
         <StorageComponent
           config={{ key, value: valueObject, type: 'session' }}
@@ -74,154 +169,112 @@ describe('useStorage', () => {
       )
     })
 
-    describe('cookie', () => {})
+    it('updates the session storage item when the value changes', () => {
+      cy.mount(
+        <StorageComponent
+          config={{ key, value, type: 'session' }}
+          toggleValue
+        />
+      )
+      cy.wait(200)
+      cy.get('[data-cy="store"]').should('have.text', value)
+      cy.get('button').click()
+      cy.get('[data-cy="store"]').should('have.text', 'new')
+    })
+  })
 
-    // it('expires the session storage item after one day', () => {
-    //   cy.clock(Date.now())
-    //   cy.window().then((win) => win.localStorage.clear())
-    //   cy.wait(1000)
-    //   cy.mount(<StorageComponent config={{ key, value, type: 'session' }} />)
-    //   cy.wait(1000)
-    //   cy.window().then((w) => {
-    //     cy.wrap(w.localStorage.getItem(key)).should('exist')
-    //   })
-    //   cy.tick(86800000)
-    //   // cy.reload(true)
-    //   cy.window().then((w) => {
-    //     cy.wrap(w.localStorage.getItem(key)).should('not.exist')
-    //   })
-    //   cy.clock().then((clock) => clock.restore())
-    // })
+  // BROWSER COOKIES
+  describe.only('cookie', () => {
+    beforeEach(() => {
+      cy.clearCookies()
+    })
 
-    // it('returns the value when present in local storage', () => {
-    //   cy.window().then((w) => {
-    //     cy.mount(<StorageComponent config={{ key, value, type: 'session' }} />)
-    //     cy.wrap(w.sessionStorage.getItem(key)).should('exist')
-    //   })
-    //   // cy.window().then((w) => {
-    //   //   cy.wrap(w.sessionStorage.getItem(key)).should('exist')
-    //   // })
-    //   cy.wait(5000)
-    //   // cy.get('[data-cy="store"]').should('have.text', 'my-key')
-    //   // cy.window().then((win) => win.sessionStorage.clear())
-    //   // cy.reload()
-    //   // cy.get('[data-cy="store"]').should('have.text', 'my-key')
-    // })
+    it('returns false when item does not exist and no value is provided', () => {
+      cy.mount(<StorageComponent config={{ key, type: 'cookie' }} />)
+      cy.wait(200)
+      cy.get('[data-cy="store"]').should('have.text', 'initial')
+    })
+
+    it('saves a new cookie when item does not exist and value is provided', () => {
+      const defaultExpiry = new Date().getTime() + 30 * 24 * 60 * 60 * 1000 // 30 days
+      cy.mount(<StorageComponent config={{ key, value, type: 'cookie' }} />)
+      cy.wait(200)
+      cy.get('[data-cy="store"]').should('have.text', value)
+      // Check for the default expiry within 2 second range
+      cy.getCookie(key).should((t) => {
+        // t.expiry returns as unix timestamp, so * 1000
+        expect(t.expiry * 1000).to.be.within(
+          defaultExpiry - 2000,
+          defaultExpiry + 2000
+        )
+      })
+    })
+
+    it('saves and serializes an object as a cookie', () => {
+      cy.mount(
+        <StorageComponent
+          config={{ key, value: valueObject, type: 'cookie' }}
+        />
+      )
+      cy.wait(200)
+      cy.get('[data-cy="store"]').should(
+        'have.text',
+        JSON.stringify(valueObject)
+      )
+    })
+
+    it('updates cookie when the value changes', () => {
+      cy.mount(
+        <StorageComponent config={{ key, value, type: 'cookie' }} toggleValue />
+      )
+      cy.wait(200)
+      cy.get('[data-cy="store"]').should('have.text', value)
+      cy.get('button').click()
+      cy.get('[data-cy="store"]').should('have.text', 'new')
+    })
+
+    it('returns false when the item expires and no value is present', () => {
+      cy.mount(
+        <StorageComponent
+          config={{
+            key,
+            value,
+            type: 'cookie',
+            expires: new Date().getTime() + 500,
+          }}
+        />
+      )
+      cy.get('[data-cy="store"]').should('have.text', value)
+      cy.wait(600)
+      cy.mount(<StorageComponent config={{ key, type: 'cookie' }} />)
+      cy.get('[data-cy="store"]').should('have.text', 'initial')
+      cy.getCookie(key).should('not.exist')
+    })
+
+    it.only('sets a new value with expiry when item has expired and value is present', () => {
+      const firstExpiry = new Date().getTime() + 500
+      cy.mount(
+        <StorageComponent
+          config={{
+            key,
+            value,
+            type: 'cookie',
+            expires: firstExpiry,
+          }}
+        />
+      )
+      cy.get('[data-cy="store"]').should('have.text', value)
+      cy.wait(600)
+      const newExpire = new Date().getTime() + 10000
+      cy.mount(
+        <StorageComponent
+          config={{ key, value: 'new', type: 'cookie', expires: newExpire }}
+        />
+      )
+      cy.get('[data-cy="store"]').should('have.text', 'new')
+      cy.getCookie(key).should((t) => {
+        expect(t.expiry * 1000).to.not.eq(firstExpiry)
+      })
+    })
   })
 })
-
-// it('should set and get a cookie value', () => {
-//   const key = 'my-key'
-//   const value = 'my-value'
-//   const options: StorageOptions = { type: 'cookie', expires: 1000 }
-//   cy.wrap(value).as('initialValue')
-
-//   cy.mount(() => {
-//     const result = useStorage(key, value, options)
-//     return <div>{result}</div>
-//   })
-
-//   cy.get('@initialValue').then((initialValue) => {
-//     cy.get('div').contains(initialValue).should('be.visible')
-//     cy.getCookie(key).should('have.property', 'value', value)
-//   })
-// })
-
-// it('should return false if the cookie has expired', () => {
-//   const key = 'my-key'
-//   const value = 'my-value'
-//   const options: StorageOptions = { type: 'cookie', expires: 1000 }
-//   cy.wrap(value).as('initialValue')
-
-//   cy.mount(() => {
-//     const result = useStorage(key, value, options)
-//     return <div>{result}</div>
-//   })
-
-//   cy.wait(1500) // Wait for the cookie to expire
-
-//   cy.get('div').contains('false').should('be.visible')
-// })
-
-// describe('useStorage', () => {
-//   beforeEach(() => {
-//     window.sessionStorage.clear()
-//     document.cookie.split(';').forEach((c) => {
-//       document.cookie = c
-//         .replace(/^ +/, '')
-//         .replace(/=.*/, `=;expires=${new Date().toUTCString()};path=/`)
-//     })
-//   })
-
-//   describe('with default options', () => {
-//     it('returns the initial value if no value is in storage', () => {
-//       const { result } = renderHook(() =>
-//         useStorage('test-key', 'initial-value')
-//       )
-//       expect(result.current).toEqual('initial-value')
-//     })
-
-//     it('returns the value from session storage if it exists', () => {
-//       window.sessionStorage.setItem('test-key', 'session-value')
-//       const { result } = renderHook(() =>
-//         useStorage('test-key', 'initial-value')
-//       )
-//       expect(result.current).toEqual('session-value')
-//     })
-
-//     it('returns the value from cookie storage if it exists', () => {
-//       document.cookie = 'test-key=cookie-value'
-//       const { result } = renderHook(() =>
-//         useStorage('test-key', 'initial-value')
-//       )
-//       expect(result.current).toEqual('cookie-value')
-//     })
-
-//     it('sets the value to session storage when it changes', () => {
-//       const { result } = renderHook(() =>
-//         useStorage('test-key', 'initial-value')
-//       )
-//       expect(window.sessionStorage.getItem('test-key')).toEqual('initial-value')
-//       result.current[1]('new-session-value')
-//       expect(window.sessionStorage.getItem('test-key')).toEqual(
-//         'new-session-value'
-//       )
-//     })
-
-//     it('sets the value to cookie storage when it changes', () => {
-//       const { result } = renderHook(() =>
-//         useStorage('test-key', 'initial-value', { type: 'cookie' })
-//       )
-//       expect(document.cookie).toContain('test-key=initial-value')
-//       result.current[1]('new-cookie-value')
-//       expect(document.cookie).toContain('test-key=new-cookie-value')
-//     })
-
-//     it('expires the session storage item after one day', () => {
-//       window.sessionStorage.setItem(
-//         'test-key',
-//         JSON.stringify({
-//           value: 'session-value',
-//           expireTime: new Date().getTime() - 1000, // make it already expired
-//         })
-//       )
-//       const { result } = renderHook(() =>
-//         useStorage('test-key', 'initial-value')
-//       )
-//       expect(result.current).toBeFalsy()
-//       expect(window.sessionStorage.getItem('test-key')).toBeFalsy()
-//     })
-
-//     it('expires the cookie after one day', () => {
-//       document.cookie = `test-key=${JSON.stringify({
-//         value: 'cookie-value',
-//         expireTime: new Date().getTime() - 1000, // make it already expired
-//       })};expires=${new Date().toUTCString()};path=/`
-//       const { result } = renderHook(() =>
-//         useStorage('test-key', 'initial-value', { type: 'cookie' })
-//       )
-//       expect(result.current).toBeFalsy()
-//       expect(document.cookie).not.toContain('test-key=cookie-value')
-//     })
-//   })
-// })
