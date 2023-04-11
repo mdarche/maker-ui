5f8cabdb4f6680915279b9996ff4aec9421afeff
mdarche <mkdarche@gmail.com>
2023-04-11 19:26:14 -0400

    Update forms build config

--- packages/forms/tsup.config.ts
+++ packages/forms/tsup.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'tsup'
 import { buildConfig } from '@maker-ui/build-tools'
 
 const config = buildConfig({
-  minifyIdentifiers: false,
-  minifyWhitespace: false,
-  minifySyntax: false,
+  minifyIdentifiers: true,
+  minifyWhitespace: true,
+  minifySyntax: true,
   entry: {
     index: 'src/index.ts',
   },
